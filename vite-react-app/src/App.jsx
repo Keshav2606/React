088d68a51be054ad7276310740c010b60d6943d9
keshav2606 <mishrag2606@gmail.com>
2024-07-29 19:42:44 +0530

    First React Learning Project

---
+++ vite-react-app/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1 id="warning">Vite + React <br /></h1>
+      <div className="card">
+        <button id="btn" className='button' onClick={() => {
+          const warning = document.getElementById('warning');
+          warning.innerHTML = "You Rascal. Don't you understand.<br/>"
+
+          const sorrybtn = document.createElement('button');
+          sorrybtn.id = 'sorry-btn'
+          sorrybtn.className = 'button'
+          sorrybtn.innerHTML = "Say Sorry"
+          sorrybtn.style.padding = '6px'
+          warning.appendChild(sorrybtn)
+          sorrybtn.addEventListener('click', ()=>{
+            warning.innerHTML= "Okay Fine."
+          })
+
+        }}>
+          Don't Click me
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
