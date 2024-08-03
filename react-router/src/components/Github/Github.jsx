ecd5c951ea43760a7956aa7d759ed7d4ad735240
keshav2606 <mishrag2606@gmail.com>
2024-08-04 00:21:32 +0530

    Checking Github API & still Incomplete.

--- react-router/src/components/Github/Github.jsx
+++ react-router/src/components/Github/Github.jsx
@@ -13,18 +13,22 @@ function Github(){
     // })
 
     const data = useLoaderData()
+    console.log("data:", data);
     return(
         <>
-        <div id="github-container flex flex-wrap flex-col gap-3">
-            <div>
-                <img src={data.avatar_url} alt="user-image" />
+        <div id="github-container" className="flex flex-wrap justify-center flex-col gap-2">
+            <div className="h-80 mt-4 mx-auto">
+                <img 
+                className="h-full"
+                src="https://avatars.githubusercontent.com/u/115573210?v=4" 
+                alt="user-image" />
             </div>
             <div id="details">
                 <h1 className= "text-blue text-3xl text-center">
-                    Name: {data.name}
+                    Keshav Kumar Mishra
                 </h1>
                 <h1 className= "text-black text-2xl text-center">
-                    Followers: {data.followers}
+                    Github Followers: {data.followers}
                 </h1>
             </div>
         </div>
@@ -36,6 +40,5 @@ export default Github
 
 export const githubInfoLoader = async () => {
     const response = await fetch(`https://api.github.com/user/keshav2606`)
-
     return response.json()
 }
