1d489d5ff6d25653925677f0b348f0d1dd76c10e
keshav2606 <mishrag2606@gmail.com>
2024-08-04 10:44:47 +0530

    Github API bug fixes.

--- react-router/src/components/Github/Github.jsx
+++ react-router/src/components/Github/Github.jsx
@@ -16,20 +16,23 @@ function Github(){
     console.log("data:", data);
     return(
         <>
-        <div id="github-container" className="flex flex-wrap justify-center flex-col gap-2">
+        <div id="github-container" className="flex flex-wrap text-center justify-center flex-col gap-2">
             <div className="h-80 mt-4 mx-auto">
                 <img 
                 className="h-full"
-                src="https://avatars.githubusercontent.com/u/115573210?v=4" 
+                src={data.avatar_url}
                 alt="user-image" />
             </div>
             <div id="details">
                 <h1 className= "text-blue text-3xl text-center">
-                    Keshav Kumar Mishra
+                   <a href={data.html_url} target="_blank" className="cursor-pointer">{data.name}</a>
                 </h1>
                 <h1 className= "text-black text-2xl text-center">
                     Github Followers: {data.followers}
+                    <a href={data.followers_url} className="text-blue-800 underline"> (See)</a>
                 </h1>
+                <a href={data.repos_url} target="_blank"
+                className="hover:text-blue-800 underline">Github Repositories</a>
             </div>
         </div>
         </>
@@ -39,6 +42,6 @@ function Github(){
 export default Github
 
 export const githubInfoLoader = async () => {
-    const response = await fetch(`https://api.github.com/user/keshav2606`)
+    const response = await fetch("https://api.github.com/users/keshav2606")
     return response.json()
 }
